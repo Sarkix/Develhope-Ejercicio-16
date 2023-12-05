23914bcf66837566191d88ae5e4455cad9ad7bac
Sarkix <142878605+Sarkix@users.noreply.github.com>
2023-12-05 19:41:20 +0100

    Configurar Prettier y Eslint.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import MouseClicker from './MouseClicker';
 
 function App() {
-  return (
-    <div>
-      <MouseClicker name="one" imageSrc="/avion-de-papel.png" />
-    </div>
-  );
+	return (
+		<div>
+			<MouseClicker name='one' imageSrc='/avion-de-papel.png' />
+		</div>
+	);
 }
 
 export default App;
-
--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -2,20 +2,26 @@ import React from 'react';
 import styles from './MouseClicker.module.css';
 
 function MouseClicker({ name, imageSrc }) {
-    const handleClick = () => {
-        console.log(`Button clicked: ${name}`)
-    }
+	const handleClick = () => {
+		console.log(`Button clicked: ${name}`);
+	};
 
-    const handleImageClick = (event) => {
-        console.log(`Image clicked: ${event.target.src}`)
-        event.stopPropagation();
-    }
+	const handleImageClick = event => {
+		console.log(`Image clicked: ${event.target.src}`);
+		event.stopPropagation();
+	};
 
-    return (
-        <button className={styles.button} name={name} onClick={handleClick}>Click me!
-        <img className={styles.image} src={imageSrc} alt={`Button Image: ${name}`} onClick={handleImageClick} />
-        </button>
-    );
+	return (
+		<button className={styles.button} name={name} onClick={handleClick}>
+			Click me!
+			<img
+				className={styles.image}
+				src={imageSrc}
+				alt={`Button Image: ${name}`}
+				onClick={handleImageClick}
+			/>
+		</button>
+	);
 }
 
 export default MouseClicker;
