e536c75d4ba527a11647739edbea5328790e0f1f
Sarkix <142878605+Sarkix@users.noreply.github.com>
2023-12-05 18:03:22 +0100

    Añadir archivos ejercicio 16 React.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MouseClicker from './MouseClicker';
+
+function App() {
+  return (
+    <div>
+      <MouseClicker name="one" imageSrc="/avion-de-papel.png" />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/MouseClicker.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './MouseClicker.module.css';
+
+function MouseClicker({ name, imageSrc }) {
+    const handleClick = () => {
+        console.log(`Button clicked: ${name}`)
+    }
+
+    const handleImageClick = (event) => {
+        console.log(`Image clicked: ${event.target.src}`)
+        event.stopPropagation();
+    }
+
+    return (
+        <button className={styles.button} name={name} onClick={handleClick}>Click me!
+        <img className={styles.image} src={imageSrc} alt={`Button Image: ${name}`} onClick={handleImageClick} />
+        </button>
+    );
+}
+
+export default MouseClicker;
